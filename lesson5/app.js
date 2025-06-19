1999369900027d470795da4770a7ee11367dc230
Bak <baktybeks@mail.ru>
2025-06-19 20:55:56 +0600

    lesson5

---
+++ lesson5/app.js
@@ -0,0 +1,107 @@
+console.log('hello lesson5')
+
+// var
+// let
+// const
+
+// let customer = 'John';
+// let customerName = 'Bakyt';
+// let customer_name = 'Kuban';
+// let $ = "12"
+// let $num = "42"
+// let _num = "45"
+// let num123123 = '234'
+// let 45 =
+// let 234sdfgsdfg =
+// let var
+// let age = 18
+// console.log('hello js', customerName, 'age', age)
+// console.log('name' + ' ' + customerName + ' ' + 'age' + ' ' + age)
+// console.log(`name = ${customerName}, rtbdrtb srthdrt age=${age}`)
+// console.log('"ОАО" Северэлектро')
+// console.log("'ОАО' Северэлектро")
+// console.log("\"ОАО\" Северэлектро")
+// console.log('d:\\imag\\nurman\\avatar.jpg')
+// let number = 123
+// let pi = 3.14
+// console.log(pi)
+// console.log(number)
+// let a = 11
+// let b = '11'
+// console.log('a=', a, 'b=', b)
+// console.log(a + b, '+')
+// console.log(a - b, '-')
+// console.log(a * b, '*')
+// console.log(a / b, '/')
+// console.log(a % b, '%')
+// console.log(a ** b, '**')
+// console.log(654654654 + '654654654654')
+// console.log('654654654' + 654654654654)
+// console.log(typeof customer);
+// console.log(typeof a);
+// let number222 = '4654275'
+// console.log(typeof Number(number222));
+// console.log(654654 + 'laskdjhf')
+// console.log(654654 * 'laskdjhf')
+// console.log(654654 * 'laskdjhf')
+// console.log(a > b, '>')
+// console.log(a < b, '<')
+// console.log(a >= b, '>=')
+// console.log(a <= b, '<=')
+// console.log(a == b, '==')
+// console.log(a === b, '===')
+// console.log(a !== b, '!==')
+// console.log(a != b, '!=')
+console.log(true && false, 'и &&')
+console.log(true && true, 'и &&')
+console.log(true && true && false, 'и &&')
+console.log(true || false || true, 'или ||')
+console.log(!true)
+console.log((true && false) || true)
+console.log(!(true && false) && true)
+console.log(15 > 10 && 'da')
+console.log(15 < 10 && 'da')
+let netu
+console.log(netu)
+let netu2 = null
+console.log(netu2)
+
+console.log(netu ?? '-')
+console.log(netu2 ?? '-')
+
+let y = 1
+console.log(y++)
+console.log(y++)
+console.log(y--)
+console.log(y)
+let x = 1
+console.log(++x)
+console.log(--x)
+console.log(x)
+
+console.log(typeof 321321)
+console.log(typeof '321321')
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof true)
+console.log(typeof Infinity)
+let a = 10009
+let b = 50
+
+let age = Number(prompt('сколько вам лет'))
+
+if (age > 1000) {
+    console.log('бессмертный')
+} else if (age > 18) {
+    console.log('совершеннолетний')
+} else {
+    console.log('не совершеннолетний')
+}
+
+let color = prompt('цвет')
+
+console.log(color)
+
+
+
